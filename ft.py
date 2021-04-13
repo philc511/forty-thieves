b32125f2023355b250439c9e8eaa571570ec1ca6
philc511 <phil.capon@fastmail.co.uk>
2021-04-13 21:35:23 +0100

    commit

--- ft.py
+++ ft.py
@@ -2,9 +2,10 @@ class Card:
     def __init__(self, suit, value):
         self.value = value
         self.suit = suit
-    def str(self):
+    def __str__(self):
         return self.value + self.suit
 
+def top(card_pile)
 
 # The type of suit
 suits = ['C', 'D', 'H', 'S']
@@ -34,15 +35,23 @@ for i in range(10):
     for j in range(4):
         stack.append(deck.pop())
     stacks.append(stack)
+    
+target_piles = []
+for i in range(8):
+    pile = []
+    target_piles.append(pile)
+
+
 
 while True:
+    print(str(deck[-1]) + "    " + " ".join([(p[-1].str() + " ") for p in target_piles]))
     print("a  b  c  d  e  f  g  h  i  j")
     max_stack = len(max(stacks, key=lambda s: len(s)))
     for i in range(max_stack):
         line = ""
         for stack in stacks:
             if (len(stack) > i):
-                line= line + stack[i].str() + " "
+                line= line + str(stack[i]) + " "
             else:
                 line = line + "   "
         print(line)
